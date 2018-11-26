40099424f17c270bc10550090c585dc626324ab4
mr-kenhoff <mrkenhoff+github@mailbox.org>
2018-11-26 17:15:39 +0100

    Update

--- dataset_manager.py
+++ dataset_manager.py
@@ -1,40 +1,14 @@
-from dataset import Dataset
+from data_backend import Dataset
 import os
-import re
+
 
 class DatasetManager:
+    def __init__(self, dataset_path):
+        self.__dataset_path = dataset_path
 
-    file_extension = ".hdf"
-
-    def __init__(self, base_path):
-        self.__base_path = base_path
-        self.datasets = []
-        self.scan_datasets()
-
-    def scan_datasets(self):
-        self.datasets = [Dataset(f) for f in os.listdir(self.__base_path) if re.match(r".*\%s" %DatasetManager.file_extension, f)]
-
-    def create_dataset(self, name, device_info):
-        """Create a new dataset if the name is not already occupied by an existing dataset"""
-        path = os.path.join(self.__base_path, name + DatasetManager.file_extension)
-        if os.path.isfile(path):
-            raise Exception("Dataset '%s' is already existing (%s)" %(name, path))
-
-        dataset = Dataset(path, device_info)
-        self.datasets.append(dataset)
-        return dataset
-
-    def get_compatible_datasets(self, device_info):
-        return [dataset for dataset in self.datasets if dataset.is_device_compatible(device_info)]
-
-
-    def get_dataset_by_name(self, name):
-        for dataset in self.datasets:
-            if (dataset.name == name):
-                return dataset
-
-        return None
-
-
-if __name__ == '__main__':
-    manager = DatasetManager("/home/jost/Projects/Controller-Backend/");
+    def get_dataset_names(self):
+        names = []
+        for filename in os.listdir(self.__dataset_path):
+            if filename.endswith(".hdf"):
+                names.append(os.path.splitext(filename)[0])
+        return names
--- server_backend.py
+++ server_backend.py
@@ -1,17 +1,82 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from data_backend import Dataset as HDF_Dataset
+from dataset_manager import DatasetManager
 
+DATASET_PATH = "./datasets"
+dataset_manager = DatasetManager(DATASET_PATH)
+API_BASE_STR = "/api/v1"
 
-class ServerBackend:
-    def __init__(self):
-        self._is_server = False
+dataset_list = []
+for dset_index, name in enumerate(dataset_manager.get_dataset_names()):
+    dset = HDF_Dataset(DATASET_PATH, name)
+    dataset_list.append({
+        "id": dset_index,
+        "name": name,
+        "device": {
+            "name": dset.device.name,
+            "version": dset.device.version
+        },
+        "subsets": [
+            {
+                "id": subset_index,
+                "name": subset,
+                "length": dset[subset].len(),
+                "link": API_BASE_STR + "/datasets/" + str(dset_index) + "/subsets/" + str(subset_index)
+            } for subset_index, subset in enumerate(dset)
+        ],
+        "link": API_BASE_STR + "/datasets/" + str(dset_index),
+    })
+    dset.close()
 
-        print(self._is_server)
+print(dataset_list)
 
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
 
-class ControllerBackend(ServerBackend):
-    def __init__(self):
-        ServerBackend.__init__(self)
-        self._is_server = True
+class DatasetList(Resource):
+    def get(self):
+        return dataset_list
 
+class Dataset(Resource):
+    def get(self, dataset_id):
+        return dataset_list[int(dataset_id)]
 
-if __name__ == '__main__':
-    moin = ControllerBackend()
+class SubsetList(Resource):
+    def get(self, dataset_id):
+        return dataset_list[int(dataset_id)]["subsets"]
+
+class Subset(Resource):
+    def get(self, dataset_id, subset_id):
+        return dataset_list[int(dataset_id)]["subsets"][int(subset_id)]
+
+class SampleList(Resource):
+    def get(self, dataset_id, subset_id):
+        dset_name = dataset_list[int(dataset_id)]["name"]
+        subset_name = dataset_list[int(dataset_id)]["subsets"][int(subset_id)]["name"]
+        dataset = HDF_Dataset(DATASET_PATH, dataset_list[int(dataset_id)]["name"])
+        return "Noch nicht implementiert"
+
+class PreprocessedSampleList(Resource):
+    def get(self, dataset_id, subset_id):
+        dset_name = dataset_list[int(dataset_id)]["name"]
+        subset_name = dataset_list[int(dataset_id)]["subsets"][int(subset_id)]["name"]
+        dataset = HDF_Dataset(DATASET_PATH, dataset_list[int(dataset_id)]["name"])
+
+        return [{
+            "id": i,
+            "lat": sample["lat"],
+            "lon": sample["lon"]
+        } for i, sample in enumerate(dataset[subset_name])]
+
+api.add_resource(DatasetList, API_BASE_STR +"/datasets")
+api.add_resource(Dataset,     API_BASE_STR + "/datasets/<dataset_id>")
+api.add_resource(SubsetList,  API_BASE_STR + "/datasets/<dataset_id>/subsets/")
+api.add_resource(Subset,      API_BASE_STR + "/datasets/<dataset_id>/subsets/<subset_id>")
+api.add_resource(SampleList,  API_BASE_STR + "/datasets/<dataset_id>/subsets/<subset_id>/samples")
+api.add_resource(PreprocessedSampleList,  API_BASE_STR + "/datasets/<dataset_id>/subsets/<subset_id>/preprocessed")
+
+if __name__ == "__main__":
+    app.run(debug=True)
